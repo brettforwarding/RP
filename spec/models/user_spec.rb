f8cf4aa09f2e38e64cdb334e9ab2cc86c624d3de
Brett <brettforwarding@gmail.com>
2012-11-06 19:03:11 -0400

    blah

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,10 +7,14 @@ end
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime        not null
-#  updated_at :datetime        not null
+#  id               :integer         not null, primary key
+#  name             :string(255)
+#  email            :string(255)
+#  created_at       :datetime        not null
+#  updated_at       :datetime        not null
+#  provider         :string(255)
+#  uid              :string(255)
+#  oauth_token      :string(255)
+#  oauth_expires_at :datetime
 #
 
