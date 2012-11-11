789f8007841097ee7a974cbdb2ec93bcde4dadd9
Brett <brettforwarding@gmail.com>
2012-11-11 18:53:15 -0400

    Finished layout and routes with facebook signin disabled

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Blog::Application.routes.draw do
   get "static_pages/home"
-
+  get "static_pages/about"
+  get "static_pages/contact"
   get "static_pages/help"
+  
+  get "users/new"
 
   root to: 'static_pages#home'
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
+  match '/signup',  to: 'users#new'
   #match '/auth/:provider/callback', :to ≥ 'sessions#create'
   #match '/auth/failure', :to ≥ 'sessions#failure'
   match '/auth/:provider/callback', to: 'sessions#create'
