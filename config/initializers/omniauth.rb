adc4b57c01314197a31a483f0c71e2f8e53a7eb6
Brett <brettforwarding@gmail.com>
2012-11-10 22:17:14 -0400

    added tutorial code up to css

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger 
  
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']#,  {:client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}#,{:scope ≥ 'email, read_stream, read_friendlists, friends_likes, friends_status, offline_access, publish_stream'}
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],  {:client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}#,{:scope ≥ 'email, read_stream, read_friendlists, friends_likes, friends_status, offline_access, publish_stream'}
 end
